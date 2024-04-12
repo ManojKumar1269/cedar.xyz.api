80381ed93cd962720e3236360082017c9d1da9f8
Manoj Kumar <159037969+ManojKumar1269@users.noreply.github.com>
2024-04-12 15:04:06 +0100

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 from common.function import PRODUCTION
 
-MONGO_SERVER = "mongodb+srv://manoj_kumar:Password123@cluster0.59iyyti.mongodb.net/" if PRODUCTION else "mongodb://localhost:27017"
+MONGO_SERVER = "mongodb+srv://mongocluster:EDIH%40123@edihmongo.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000" if PRODUCTION else "mongodb://localhost:27017"
 
 def database():
   client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_SERVER)
