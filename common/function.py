2279a75a0b4db6465fd39fa89f4082083e594d17
Manoj Kumar <159037969+ManojKumar1269@users.noreply.github.com>
2024-04-12 15:06:09 +0100

    Update function.py

--- common/function.py
+++ common/function.py
@@ -5,7 +5,7 @@ import pickle
 import yaml
 from rag.loadData import initialize_splitter
 
-PRODUCTION = False
+PRODUCTION = True
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
